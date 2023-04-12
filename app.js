243a306957f33ef2101c2c329f12c52878cad32e
Steven Kwan <stevent.kwan@gmail.com>
2023-04-12 11:31:52 -0700

    added preflight request handler and an api route to get current price of a stock symbol

--- app.js
+++ app.js
@@ -4,8 +4,45 @@ const app = express();
 // Make env variables available
 const dotenv = require('dotenv').config();
 
+// CORS preflight OPTIONS request
+const handlePreflight = function (req, res, next) {
+  // CORS headers
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  // Set custom headers for CORS
+  res.header("Access-Control-Allow-Headers", "Content-Type,Accept,X-Custom-Header");
+
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+
+  return next();
+};
+
+app.use(handlePreflight);
+
 app.get('/', (req, res, next) => {
   res.send('Hello world!');
+});
+
+app.get('/api/quote/:sym', async (req, res, next) => {
+  const symbol = req.params.sym;
+  try {
+    let response = await fetch('https://finnhub.io/api/v1/quote?symbol=' + symbol, {
+      method: 'GET',
+      headers: {
+        "X-Finnhub-Token": process.env.finnhubAPIKey
+      }
+    });
+
+    if (response.ok) {
+      let data = await response.json();
+      res.json({data});
+    }
+
+  } catch (err) {
+    res.json({err});
+  }
 });
 
 app.listen(process.env.PORT, () => {
