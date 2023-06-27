a25bbc29c7625ea7782e339124e1262eec48f90e
Steven Kwan <stevent.kwan@gmail.com>
2023-06-27 04:13:11 -0700

    updated preflight

--- config/handlePreFlight.js
+++ config/handlePreFlight.js
@@ -1,7 +1,7 @@
 // CORS preflight OPTIONS request
 export default function handlePreFlight(req, res, next) {
   // CORS headers
-  res.header("Access-Control-Allow-Origin", "https://primestock-app.vercel.app"); // restrict it to the required domain
+  res.header("Access-Control-Allow-Origin", "https://primestock.vercel.app"); // restrict it to the required domain
   res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,PATCH,OPTIONS");
   // Set custom headers for CORS
   res.header("Access-Control-Allow-Headers", "Content-Type,Accept,X-Custom-Header,Authorization");
